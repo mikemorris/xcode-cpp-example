02f200cf02233d925b80fadc68c735e4231281ac
Mike Morris <michael.patrick.morris@gmail.com>
2015-10-14 17:57:40 -0400

    Initial Commit

---
+++ xcode-cpp-example/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  xcode-cpp-example
+//
+//  Created by Mike Morris on 10/14/15.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
